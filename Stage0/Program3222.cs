1e8651ba7f8fb06365de783725eecb183c2c2f9c
ShulamitGreensfeld <Hand4d32@mbjcomp.org.il>
2024-10-28 23:18:48 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program3222.cs
@@ -0,0 +1,18 @@
+﻿namespace Stage0;
+partial class Program
+{
+    private static void Main()
+    {
+        welcome3222();
+        welcome5197();
+        Console.ReadKey();
+    }
+
+    private static void welcome3222()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+        private static void welcome5197() { }
+}
