c9a00abc5b9be2962c5c28e512e177c557bae16d
אוסטרוב מאשא רחל <Hand4d1@mbjcomp.org.il>
2024-10-28 23:47:16 +0200

    "New class + Welcome functions"

--- Stage0/Program3222.cs
+++ Stage0/Program3222.cs
@@ -14,10 +14,5 @@ partial class Program
         string name = Console.ReadLine();
         Console.WriteLine("{0}, welcome to my first console application", name);
     }
-        private static void welcome5197() 
-    {
-        Console.WriteLine("Enter your name:");
-        string name = Console.ReadLine();
-        Console.WriteLine("{0}, welcome to my first console application", name);
-    } 
+        private static void welcome5197() { } 
 }
---
+++ Stage0/Program5197.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Stage0;
+
+partial class Program
+{
+	public Program()
+	{
+		static partial void welcome5197()
+	{
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+
+    }
+}
