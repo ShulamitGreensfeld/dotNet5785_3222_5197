184471aed90e30e61766a9e55f5cfc27cc3e6438
אוסטרוב מאשא רחל <Hand4d1@mbjcomp.org.il>
2024-12-11 23:03:53 +0200

    tell the middle of chapter 6

---
+++ DalXml/Config.cs
@@ -0,0 +1,46 @@
+﻿namespace Dal;
+internal static class Config
+{
+    internal const string s_data_config_xml = "data-config.xml";
+    internal const string s_volunteers_xml = "volunteers.xml";
+    internal const string s_calls_xml = "calls.xml";
+    internal const string s_assignments_xml = "assignments.xml";
+
+    internal static int NextCallId
+    {
+        get => XMLTools.GetAndIncreaseConfigIntVal(s_data_config_xml, "NextCallId");
+        private set => XMLTools.SetConfigIntVal(s_data_config_xml, "NextCallId", value);
+    }
+
+
+    internal static int NextAssignmentId
+    {
+        get => XMLTools.GetAndIncreaseConfigIntVal(s_data_config_xml, "nextAssignmentId");
+        private set => XMLTools.SetConfigIntVal(s_data_config_xml, "nextAssignmentId", value);
+    }
+
+
+
+    internal static DateTime Clock
+    {
+        get => XMLTools.GetConfigDateVal(s_data_config_xml, "Clock");
+        set => XMLTools.SetConfigDateVal(s_data_config_xml, "Clock", value);
+    }
+
+    internal static TimeSpan RiskRange
+    {
+        get => XMLTools.GetConfigTimeSpanVal(s_data_config_xml, "RiskRange");
+        set => XMLTools.SetConfigTimeSpanVal(s_data_config_xml, "RiskRange", value);
+    }
+
+    /// <summary>
+    /// מתודה לאיפוס ערכים התחלתיים
+    /// </summary>
+    internal static void Reset()
+    {
+        NextCallId = 1000;
+        NextAssignmentId = 2000;
+        Clock = DateTime.Now;
+        RiskRange = TimeSpan.FromHours(2); // הגדרת ערך ברירת מחדל ל-RiskRange
+    }
+}
